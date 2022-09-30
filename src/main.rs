36d4bd96c5dcf13186492092a85ea9edbef34cdd
Ignacio Gomez <ignacio.gomez.arboleda@gmail.com>
2022-09-30 11:39:59 +0200

    server with internal state

--- src/main.rs
+++ src/main.rs
@@ -1,106 +1,40 @@
-extern crate reqwest;
-extern crate serde;
-extern crate serde_json;
-extern crate url;
+#![feature(proc_macro_hygiene, decl_macro)]
 
-use std::{io::Write, net::TcpListener};
+#[macro_use]
+extern crate rocket;
 
-pub mod pkg;
-#[tokio::main]
-async fn main() {
-    println!("\n");
+use rocket::State;
+use std::sync::Mutex;
 
-    println!("Hello, world!");
-
-    let usera = User {
-        id: 1,
-        email: "someemail".to_string(),
-        name: "ignacio".to_string(),
-        password: "password".to_string(),
-    };
-
-    println!("{}", usera.name);
-    let somedog = Dog {
-        id: 1,
-        name: "some".to_string(),
-        age: 1,
-        breed: "some".to_string(),
-    };
-
-    make_walk(somedog);
-    make_walk(usera);
-
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    println!("Listening on port 7878");
-    for stream in listener.incoming() {
-        if stream.is_err() {
-            println!("Error: {}", stream.unwrap_err());
-            continue;
-        }
-        let mut stream = stream.unwrap();
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = "Hello, World!";
-        let length = contents.len();
-
-        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-        stream.write_all(response.as_bytes()).unwrap();
-
-        println!("Connection established!");
-        drop(stream)
-    }
+struct SharedData {
+    numbers: Mutex<Vec<i32>>,
 }
 
-struct User {
-    id: i32,
-    name: String,
-    email: String,
-    password: String,
+#[get("/count")]
+fn get_number(shared: State<SharedData>) -> String {
+    let shared_data: &SharedData = shared.inner();
+    // shared_data.numbers.lock().unwrap().push(number);
+    format!(
+        "There are now {} numbers",
+        shared_data.numbers.lock().unwrap().len()
+    )
 }
 
-struct Dog {
-    id: i32,
-    name: String,
-    age: i32,
-    breed: String,
-}
-// walks a distance in a direction
-trait Walker {
-    fn walk(&self, distance: i32) -> i32;
-}
-
-trait Runner {
-    fn run(&self, distance: i32) -> i32;
+#[get("/add")]
+fn add_number(shared: State<SharedData>) -> String {
+    let shared_data: &SharedData = shared.inner();
+    shared_data.numbers.lock().unwrap().push(1);
+    format!(
+        "There are now {} numbers",
+        shared_data.numbers.lock().unwrap().len()
+    )
 }
 
-impl Walker for User {
-    fn walk(&self, distance: i32) -> i32 {
-        println!("im a user and i move {} units", distance);
-        return 10;
-    }
-}
-
-impl Walker for Dog {
-    fn walk(&self, distance: i32) -> i32 {
-        println!("im a dog and i move {} units", distance);
-        return 10;
-    }
-}
-
-impl Runner for User {
-    fn run(&self, distance: i32) -> i32 {
-        println!("im a user and i run {} units", distance);
-        return 10;
-    }
-}
-
-impl Runner for Dog {
-    fn run(&self, distance: i32) -> i32 {
-        println!("im a dog and i run {} units", distance);
-        return 10;
-    }
-}
-
-fn make_walk(walker: impl Walker) {
-    walker.walk(10);
+fn main() {
+    rocket::ignite()
+        .mount("/", routes![get_number, add_number])
+        .manage(SharedData {
+            numbers: Mutex::new(vec![5]),
+        })
+        .launch();
 }
